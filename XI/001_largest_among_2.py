250fe246fae4a0b56cb49353ff4a1f7926123f81
Dhruvaraja Kunjar <dhruvaraja.k@gmail.com>
2022-03-26 20:19:37 +0530

    Create 001_largest_among_2.py
    Input two numbers and display the larger / smaller number.

---
+++ XI/001_largest_among_2.py
@@ -0,0 +1,12 @@
+# Input two numbers and display the larger / smaller number.
+
+print("Enter 2 numbers")
+a = int(input("Enter First number"))
+b = int(input("Enter Second number"))
+
+if (a > b):
+      print(a,"is", "Largest number")
+      print(b, "is", "Smallest number")
+else:
+    print("{} is Largest number".format(b))
+    print("{} is Smallest number".format(a))
