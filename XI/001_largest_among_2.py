c4219ed5b7db94ecc7c4895912017cd58e8d1b68
Dhruvaraja Kunjar <dhruvaraja.k@gmail.com>
2022-03-26 21:00:01 +0530

    Update 001_largest_among_2.py
    Ran 'Black' to fix formatting issues

--- XI/001_largest_among_2.py
+++ XI/001_largest_among_2.py
@@ -4,9 +4,9 @@ print("Enter 2 numbers")
 a = int(input("Enter First number"))
 b = int(input("Enter Second number"))
 
-if (a > b):
-      print(a,"is", "Largest number")
-      print(b, "is", "Smallest number")
+if a > b:
+    print(a, "is", "Largest number")
+    print(b, "is", "Smallest number")
 else:
     print("{} is Largest number".format(b))
     print("{} is Smallest number".format(a))
